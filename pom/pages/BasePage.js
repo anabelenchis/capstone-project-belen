64a3b692df6762b18ca5e65a1cfdc6d0675284a3
anabelenchis <anabelen.garcia@wizeline.com>
2021-07-20 18:43:14 -0500

    Update tasks tests

--- pom/pages/BasePage.js
+++ pom/pages/BasePage.js
@@ -1,9 +1,42 @@
-import { Selector } from "testcafe"
+import { Selector, t } from "testcafe"
 
 class BasePage {
     constructor() {
         this.pageTitle = Selector('h1').withText('Today')
         this.addButton = Selector('.plus_add_button').withText('Add task')
+        this.dateButton = Selector('.date.date_today')
+        this.tomorrowDate = Selector('.scheduler-suggestions-item-label').withText('Tomorrow')
+        this.taskTitleField = Selector('.public-DraftStyleDefault-block')
+        this.taskDescriptionField = Selector('.task_editor__description_field')
+        this.submitTaskButton = Selector('.reactist_button')
+        this.taskTitle = Selector('task_content')
+        this.taskDescription = Selector('task_description')
+
+    }
+
+    async createTask(numberOfTasks, date) {
+        await t.click(this.addButton)
+        for (var i = 0; i < numberOfTasks; i++) {
+            await t
+                .typeText(this.taskTitleField, 'Task number ' + (i + 1), { paste: true })
+                console.log(date)
+            date == 'tomorrow' ?
+                await t
+                    .click(this.dateButton)
+                    .click(this.tomorrowDate)
+                    .click(this.submitTaskButton)
+                    .wait(1500)
+                : await t.click(this.submitTaskButton).wait(1500)
+        }
+        /*
+        .click(basePage.addButton)
+                .typeText('.public-DraftStyleDefault-block', 'hello world', {paste: true})
+                .typeText('.task_editor__description_field', 'This is the task description', {paste: true})
+                .click('.reactist_button')
+                .wait(1500)
+                .expect(Selector('.task_content').innerText).contains('hello world')
+                .expect(Selector('.task_description').innerText).contains('This is the task description')
+        */
     }
 }
 
--- pom/tests/Tasks.test.js
+++ pom/tests/Tasks.test.js
@@ -4,12 +4,21 @@
 import {STANDARD_USER} from '../data/Roles'
 import {CREDENTIALS, URLS, MESSAGES} from '../data/Constants'
 import basePage from '../pages/BasePage'
+import { Selector } from 'testcafe'
 
 fixture ('Tasks creation tests').page `${URLS.LOGIN_URL}`
 
 test.only('As a user I want to add a new task with Today as the due date', async t => {
     await t.useRole(STANDARD_USER)
-    await t.click(basePage.addButton)
+    await basePage.createTask(3, 'tomorrow')
+    /*
+    await t
+        .click(basePage.addButton)
+        .typeText('.public-DraftStyleDefault-block', 'hello world', {paste: true})
+        .typeText('.task_editor__description_field', 'This is the task description', {paste: true})
+        .click('.reactist_button')
+        .wait(1500)
+        .expect(Selector('.task_content').innerText).contains('hello world')
+        .expect(Selector('.task_description').innerText).contains('This is the task description')
+        */
 })
-
-
