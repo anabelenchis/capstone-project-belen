2cd8c52eb7c2e11dfb89e737c8fa95e530d3ab16
anabelenchis <anabelen.garcia@wizeline.com>
2021-07-21 17:07:45 -0500

    Add smoke test script

--- pom/tests/Projects.test.js
+++ pom/tests/Projects.test.js
@@ -4,7 +4,7 @@ import addProjectPage from '../pages/AddProjectPage'
 
 fixture('Projects creations tests').page`${URLS.LOGIN_URL}`
 
-test.only('As a user I want to create a new project and add it to my favorites', async t => {
+test.meta('type','smoke')('As a user I want to create a new project and add it to my favorites', async t => {
     await t.useRole(STANDARD_USER)
     await addProjectPage.createProject()
     await t.expect(await addProjectPage.assertProjectCreation()).ok()
--- pom/tests/Tasks.test.js
+++ pom/tests/Tasks.test.js
@@ -5,14 +5,14 @@ import upcomingPage from '../pages/UpcomingPage'
 
 fixture('Tasks creation tests').page`${URLS.LOGIN_URL}`
 
-test.skip('As a user I want to add a new task with Today as the due date', async t => {
+test.meta('type','smoke')('As a user I want to add a new task with Today as the due date', async t => {
     await t.useRole(STANDARD_USER)
     await todayPage.createTask(NUMBER_OF_TASKS.MINIMUM, DATE.TODAY)
     await t.expect(await todayPage.assertTasksCreated(NUMBER_OF_TASKS.MINIMUM)).ok()
     await todayPage.clearTodayPage(NUMBER_OF_TASKS.MINIMUM)
 })
 
-test.skip('As a user I want to add a new task with Tomorrow as the due date', async t => {
+test('As a user I want to add a new task with Tomorrow as the due date', async t => {
     await t.useRole(STANDARD_USER)
     await todayPage.createTask(NUMBER_OF_TASKS.MINIMUM, DATE.TOMORROW)
     await t.expect(await upcomingPage.assertTasksCreated(NUMBER_OF_TASKS.MINIMUM)).ok()
@@ -20,7 +20,7 @@ test.skip('As a user I want to add a new task with Tomorrow as the due date', as
 
 })
 
-test.skip('As a user I want to add 10 new tasks with Today as the due date', async t => {
+test('As a user I want to add 10 new tasks with Today as the due date', async t => {
     await t.useRole(STANDARD_USER)
     await todayPage.createTask(NUMBER_OF_TASKS.MAXIMUM, DATE.TODAY)
     await t.expect(await todayPage.assertTasksCreated(NUMBER_OF_TASKS.MAXIMUM)).ok()
