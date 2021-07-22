1149da0c1b824d2e8082ff99d113e7585e9aee17
anabelenchis <anabelen.garcia@wizeline.com>
2021-07-22 09:01:48 -0500

    Add fixtures

--- pom/tests/Projects.test.js
+++ pom/tests/Projects.test.js
@@ -2,10 +2,13 @@ import { STANDARD_USER } from '../data/Roles'
 import { URLS } from '../data/Constants'
 import addProjectPage from '../pages/AddProjectPage'
 
-fixture('Projects creations tests').page`${URLS.LOGIN_URL}`
+fixture('Projects creations tests')
+    .page`${URLS.LOGIN_URL}`
+    .beforeEach ( async t => {
+        await t.useRole(STANDARD_USER)
+    })
 
 test.meta('type','smoke')('As a user I want to create a new project and add it to my favorites', async t => {
-    await t.useRole(STANDARD_USER)
     await addProjectPage.createProject()
     await t.expect(await addProjectPage.assertProjectCreation()).ok()
     await addProjectPage.cleanUpProjects()
--- pom/tests/Tasks.test.js
+++ pom/tests/Tasks.test.js
@@ -3,17 +3,20 @@ import { URLS, DATE, NUMBER_OF_TASKS } from '../data/Constants'
 import todayPage from '../pages/TodayPage'
 import upcomingPage from '../pages/UpcomingPage'
 
-fixture('Tasks creation tests').page`${URLS.LOGIN_URL}`
+fixture('Tasks creation tests')
+    .page`${URLS.LOGIN_URL}`
+    .beforeEach ( async t => {
+        await t.useRole(STANDARD_USER)
+    })
+
 
 test.meta('type','smoke')('As a user I want to add a new task with Today as the due date', async t => {
-    await t.useRole(STANDARD_USER)
     await todayPage.createTask(NUMBER_OF_TASKS.MINIMUM, DATE.TODAY)
     await t.expect(await todayPage.assertTasksCreated(NUMBER_OF_TASKS.MINIMUM)).ok()
     await todayPage.clearTodayPage(NUMBER_OF_TASKS.MINIMUM)
 })
 
 test('As a user I want to add a new task with Tomorrow as the due date', async t => {
-    await t.useRole(STANDARD_USER)
     await todayPage.createTask(NUMBER_OF_TASKS.MINIMUM, DATE.TOMORROW)
     await t.expect(await upcomingPage.assertTasksCreated(NUMBER_OF_TASKS.MINIMUM)).ok()
     await upcomingPage.clearUpcomingPage(NUMBER_OF_TASKS.MINIMUM)
@@ -21,7 +24,6 @@ test('As a user I want to add a new task with Tomorrow as the due date', async t
 })
 
 test('As a user I want to add 10 new tasks with Today as the due date', async t => {
-    await t.useRole(STANDARD_USER)
     await todayPage.createTask(NUMBER_OF_TASKS.MAXIMUM, DATE.TODAY)
     await t.expect(await todayPage.assertTasksCreated(NUMBER_OF_TASKS.MAXIMUM)).ok()
     await todayPage.clearTodayPage(NUMBER_OF_TASKS.MAXIMUM)
