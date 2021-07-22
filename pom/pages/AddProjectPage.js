7c87b5118ebe4fe7fa6ba2b609ed7d1233dbdd85
anabelenchis <anabelen.garcia@wizeline.com>
2021-07-22 16:37:05 -0500

    add double click

--- pom/pages/AddProjectPage.js
+++ pom/pages/AddProjectPage.js
@@ -21,8 +21,7 @@ class AddProjectPage {
 
     async createProject() {
         await t
-            .click(basePage.addProject).wait(1000)
-            .click(basePage.addProject).wait(1000)
+            .doubleClick(basePage.addProject)
             .typeText(this.projectTitle, PROJECT_ATTRIBUTES.PROJECT_TITLE)
             .click(this.colorSelectionDropdown)
             .click(this.colorSelectionButton)
