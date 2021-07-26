3230da38fcf623b44cde50c88568df8c2d792f07
anabelenchis <anabelen.garcia@wizeline.com>
2021-07-26 10:59:54 -0500

    refactor

--- pom/tests/Projects.test.js
+++ pom/tests/Projects.test.js
@@ -11,5 +11,7 @@ fixture('Projects creations tests')
 test.meta('type', 'smoke')('As a user I want to create a new project and add it to my favorites', async t => {
     await addProjectPage.createProject()
     await t.expect(await addProjectPage.assertProjectCreation()).ok()
-    await addProjectPage.cleanUpProjects()
 })
+    .after(async t => {
+        await addProjectPage.cleanUpProjects()
+    })
--- pom/tests/Tasks.test.js
+++ pom/tests/Tasks.test.js
@@ -13,18 +13,23 @@ fixture('Tasks creation tests')
 test.meta('type', 'smoke')('As a user I want to add a new task with Today as the due date', async t => {
     await todayPage.createTask(NUMBER_OF_TASKS.MINIMUM, DATE.TODAY)
     await t.expect(await todayPage.assertTasksCreated(NUMBER_OF_TASKS.MINIMUM)).ok()
-    await todayPage.clearTodayPage(NUMBER_OF_TASKS.MINIMUM)
 })
+    .after(async t => {
+        await todayPage.clearTodayPage(NUMBER_OF_TASKS.MINIMUM)
+    })
 
 test('As a user I want to add a new task with Tomorrow as the due date', async t => {
     await todayPage.createTask(NUMBER_OF_TASKS.MINIMUM, DATE.TOMORROW)
     await t.expect(await upcomingPage.assertTasksCreated(NUMBER_OF_TASKS.MINIMUM)).ok()
-    await upcomingPage.clearUpcomingPage(NUMBER_OF_TASKS.MINIMUM)
-
 })
+    .after(async t => {
+        await upcomingPage.clearUpcomingPage(NUMBER_OF_TASKS.MINIMUM)
+    })
 
 test('As a user I want to add 10 new tasks with Today as the due date', async t => {
     await todayPage.createTask(NUMBER_OF_TASKS.MAXIMUM, DATE.TODAY)
     await t.expect(await todayPage.assertTasksCreated(NUMBER_OF_TASKS.MAXIMUM)).ok()
-    await todayPage.clearTodayPage(NUMBER_OF_TASKS.MAXIMUM)
 })
+    .after(async t => {
+        await todayPage.clearTodayPage(NUMBER_OF_TASKS.MAXIMUM)
+    })
