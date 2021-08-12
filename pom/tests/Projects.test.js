8a8ed19f00eae9d41a851cb2af3cf89af3074b0e
anabelenchis <anabelen.garcia@wizeline.com>
2021-08-12 11:33:53 -0500

    second review clean up

--- pom/tests/Projects.test.js
+++ pom/tests/Projects.test.js
@@ -14,7 +14,7 @@ test
     .after(async (t) => {
         await t.wait(WAIT.LOADPAGE)
     })
-    .only.meta('type', 'smoke')('As a user I want to create a new project and add it to my favorites', async (t) => {
+    .meta('type', 'smoke')('As a user I want to create a new project and add it to my favorites', async (t) => {
         await basePage.createProject(PROJECT_ATTRIBUTES.PROJECT_TITLE, PROJECT_ATTRIBUTES.PROJECT_COLOR, PROJECT_ATTRIBUTES.FAVORITE_PROJECT)
         await t.click(basePage.favoriteListItem.withText(PROJECT_ATTRIBUTES.PROJECT_TITLE))
         await t.expect(await projectPage.assertProjectCreation(PROJECT_ATTRIBUTES.PROJECT_TITLE, PROJECT_ATTRIBUTES.PROJECT_COLOR, PROJECT_ATTRIBUTES.FAVORITE_PROJECT)).ok()
